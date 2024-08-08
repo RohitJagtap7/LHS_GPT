9eceb9351ff4f7056233ca2fdbfee95725501287
RohitJagtap7 <rohitjagtap7020450327@gmail.com>
2024-08-08 09:56:22 +0530

    Update app6.py

--- app6.py
+++ app6.py
@@ -131,7 +131,7 @@ st.markdown("""
             margin-top: 20px;
         }           
         #MainMenu{
-            display:none;
+            
         }
         #baseButton-header {
             display:none;
