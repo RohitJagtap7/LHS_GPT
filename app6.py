c8528fd5b59b961534e806570c3a85dcaaaef2ef
RohitJagtap7 <rohitjagtap7020450327@gmail.com>
2024-08-08 10:09:39 +0530

    Update app6.py

--- app6.py
+++ app6.py
@@ -136,7 +136,7 @@ st.markdown("""
         .stActionButton{
             display:none;
         }
-        .viewerBadge_link__qRIco{
+        .viewerBadge_container__r5tak styles_viewerBadge__CvC9N{
             display:none;
         }
     </style>
