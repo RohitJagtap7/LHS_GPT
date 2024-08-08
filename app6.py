14fa8a2bac4afe0432e5888fdce67e7bd2803bb2
RohitJagtap7 <rohitjagtap7020450327@gmail.com>
2024-08-08 13:18:05 +0530

    Update app6.py

--- app6.py
+++ app6.py
@@ -99,7 +99,7 @@ st.markdown("""
             top: 0;
             width: 75%;
             height: 150px;
-            background-color: #fff; /* Change to desired background color */
+            background-color:; /* Change to desired background color */
             z-index: 1000;
             display: flex;
             align-items: center;
